682485804aed373b96c7fdcb9616fcdb1ba8a0da
Ange-H0610 <hei.iraka.4@gmail.com>
2026-03-19 09:28:09 +0300

    Initial commit - Projet Spring Boot généré

---
+++ tdstudent/src/main/java/com/example/tdstudent/TdstudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdstudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdstudentApplication.class, args);
+	}
+
+}
---
+++ tdstudent/src/test/java/com/example/tdstudent/TdstudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdstudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
